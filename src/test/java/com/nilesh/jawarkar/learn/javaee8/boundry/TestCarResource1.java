5f12f8257ecb3f7a9571f48e1fbc93c8cf004ab0
nileshjawarkar <jawarkar.nilesh@gmail.com>
2023-01-15 00:58:56 +0530

    arquillian test for Resource 01 and 02.

---
+++ src/test/java/com/nilesh/jawarkar/learn/javaee8/boundry/TestCarResource1.java
@@ -0,0 +1,139 @@
+package com.nilesh.jawarkar.learn.javaee8.boundry;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.net.URL;
+import java.util.Date;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.nilesh.jawarkar.learn.javaee8.entity.Car;
+import com.nilesh.jawarkar.learn.javaee8.entity.Color;
+import com.nilesh.jawarkar.learn.javaee8.entity.EngineType;
+
+@RunWith(Arquillian.class)
+public class TestCarResource1 {
+	static long startTime = 0;
+
+	@Deployment
+	public static WebArchive createDeployment() {
+		return ShrinkWrap.create(WebArchive.class, "carman.war")
+		        .addPackage("com.nilesh.jawarkar.learn.javaee8.boundry")
+		        .addPackage("com.nilesh.jawarkar.learn.javaee8.config")
+		        .addPackage("com.nilesh.jawarkar.learn.javaee8.control")
+		        .addPackage("com.nilesh.jawarkar.learn.javaee8.entity")
+		        .addAsResource("persistence.xml", "META-INF/persistence.xml")
+		        .addAsResource("beans.xml", "META-INF/beans.xml");
+	}
+
+	@AfterClass
+	public static void endForAll() {
+		final long endTime = new Date().getTime();
+		System.out.println("Time taken by all tests - " + (endTime - startTime));
+	}
+
+	@BeforeClass
+	public static void startForAll() {
+		startTime = new Date().getTime();
+	}
+
+	@PersistenceContext
+	EntityManager   entityManager;
+
+	@Inject
+	UserTransaction utx;
+
+	@ArquillianResource
+	private URL     url;
+
+	WebTarget       createAndQueryTarget = null;
+	Client          client               = null;
+
+	@After
+	public void cleanUp() {
+		client.close();
+	}
+
+	@Before
+	public void init() throws NotSupportedException, SystemException,
+	        IllegalStateException, SecurityException, HeuristicMixedException,
+	        HeuristicRollbackException, RollbackException {
+		utx.begin();
+		entityManager.joinTransaction();
+		System.out.println("Dumping old records...");
+		entityManager.createQuery("delete from Car").executeUpdate();
+		utx.commit();
+
+		String       strURL    = "http://localhost:8080/carman/resources/cars";
+		final String startPort = System.getProperty("tomee.httpPort");
+		if (url != null) {
+			strURL = url.toString() + "/resources/cars";
+		} else if (startPort != null) {
+			strURL = "http://localhost:" + startPort + "/carman/resources/cars";
+		}
+		System.out.println("URL = " + strURL);
+		client               = ClientBuilder.newClient();
+		createAndQueryTarget = client.target(strURL);
+	}
+
+	@Test
+	public void should_create_and_retrieve_car() {
+		final JsonObject obj     = Json.createObjectBuilder()
+		        .add("engineType", EngineType.DIESEL.name())
+		        .add("color", Color.BLUE.name()).build();
+
+		Response         postRes = createAndQueryTarget
+		        .request(MediaType.APPLICATION_JSON).post(Entity.json(obj));
+		final Car        car01   = postRes.readEntity(Car.class);
+		assertNotNull(car01);
+
+		postRes = createAndQueryTarget.request(MediaType.APPLICATION_JSON)
+		        .post(Entity.json(obj));
+		assertNotNull(postRes);
+		final Car car02 = postRes.readEntity(Car.class);
+		assertNotNull(car02);
+
+		final Response getRes = createAndQueryTarget.request(MediaType.APPLICATION_JSON)
+		        .get();
+		assertNotNull(getRes);
+
+		final GenericType<List<Car>> genCarList = new GenericType<List<Car>>() {
+												};
+		final List<Car>              carList    = getRes.readEntity(genCarList);
+		assertNotNull(carList);
+		assertEquals(2, carList.size());
+		carList.forEach(c -> System.out.println(c.getId()));
+
+	}
+}
---
+++ src/test/java/com/nilesh/jawarkar/learn/javaee8/boundry/TestCarResource2.java
@@ -0,0 +1,178 @@
+package com.nilesh.jawarkar.learn.javaee8.boundry;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.net.URL;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.nilesh.jawarkar.learn.javaee8.entity.Car;
+import com.nilesh.jawarkar.learn.javaee8.entity.Color;
+import com.nilesh.jawarkar.learn.javaee8.entity.EngineType;
+
+@RunWith(Arquillian.class)
+public class TestCarResource2 {
+	static long startTime = 0;
+
+	@Deployment
+	public static WebArchive createDeployment() {
+		return ShrinkWrap.create(WebArchive.class, "carman.war")
+		        .addPackage("com.nilesh.jawarkar.learn.javaee8.boundry")
+		        .addPackage("com.nilesh.jawarkar.learn.javaee8.config")
+		        .addPackage("com.nilesh.jawarkar.learn.javaee8.control")
+		        .addPackage("com.nilesh.jawarkar.learn.javaee8.entity")
+		        .addAsResource("persistence.xml", "META-INF/persistence.xml")
+		        .addAsResource("beans.xml", "META-INF/beans.xml");
+	}
+
+	@AfterClass
+	public static void endForAll() {
+		final long endTime = new Date().getTime();
+		System.out.println("Time taken by all tests - " + (endTime - startTime));
+	}
+
+	@BeforeClass
+	public static void startForAll() {
+		startTime = new Date().getTime();
+	}
+
+	/*
+	 * @PersistenceContext EntityManager entityManager;
+	 *
+	 * @Inject UserTransaction utx;
+	 */
+
+	@ArquillianResource
+	private URL url;
+
+	WebTarget   createAndQueryTarget = null;
+	WebTarget   queryTarget          = null;
+	Client      client               = null;
+
+	@After
+	public void cleanUp() {
+		client.close();
+	}
+
+	@Before
+	public void init() /*
+	                    * throws NotSupportedException, SystemException,
+	                    * IllegalStateException, SecurityException,
+	                    * HeuristicMixedException, HeuristicRollbackException,
+	                    * RollbackException
+	                    */ {
+		/*
+		 * utx.begin(); entityManager.joinTransaction();
+		 * System.out.println("Dumping old records...");
+		 * entityManager.createQuery("delete from Car").executeUpdate(); utx.commit();
+		 */
+
+		String       strURL    = "http://localhost:8080/carman/resources/v2/cars";
+		final String startPort = System.getProperty("tomee.httpPort");
+		if (url != null) {
+			strURL = url.toString() + "/resources/v2/cars";
+		} else if (startPort != null) {
+			strURL = "http://localhost:" + startPort + "/carman/resources/v2/cars";
+		}
+		System.out.println("URL = " + strURL);
+		client               = ClientBuilder.newClient();
+		createAndQueryTarget = client.target(strURL);
+		queryTarget          = client.target(strURL + "?attr={attr}&value={value}");
+	}
+
+	@Test
+	public void should_create_and_retrieve_car() {
+
+		// -- Create 2 blue cars
+		final JsonObject blueCarJO = Json.createObjectBuilder()
+		        .add("engineType", EngineType.DIESEL.name())
+		        .add("color", Color.BLUE.name()).build();
+
+		// -- 1 blue car
+		Response         postRes   = createAndQueryTarget
+		        .request(MediaType.APPLICATION_JSON).post(Entity.json(blueCarJO));
+		final Car        car01     = postRes.readEntity(Car.class);
+		assertNotNull(car01);
+
+		// -- 2 blue car
+		postRes = createAndQueryTarget.request(MediaType.APPLICATION_JSON)
+		        .post(Entity.json(blueCarJO));
+		assertNotNull(postRes);
+		final Car car02 = postRes.readEntity(Car.class);
+		assertNotNull(car02);
+
+		// --- Get all the cars.. must be 2
+		final Response getRes = createAndQueryTarget.request(MediaType.APPLICATION_JSON)
+		        .get();
+		assertNotNull(getRes);
+
+		// -- Using generic type as we can not use List<Car>.class
+		final GenericType<List<Car>> genCarList = new GenericType<List<Car>>() {
+												};
+		final List<Car>              carList    = getRes.readEntity(genCarList);
+		assertNotNull(carList);
+		assertEquals(2, carList.size());
+		carList.forEach(c -> System.out.println(c.getId()));
+
+		// -- create 1 red car
+		final JsonObject redCarJO = Json.createObjectBuilder()
+		        .add("engineType", EngineType.PETROL.name())
+		        .add("color", Color.RED.name()).build();
+
+		postRes = createAndQueryTarget.request(MediaType.APPLICATION_JSON)
+		        .post(Entity.json(redCarJO));
+		assertNotNull(postRes);
+		final Car car03 = postRes.readEntity(Car.class);
+		assertNotNull(car03);
+
+		// -- Retrieve red cars - must be 1
+		final Response  getResRed  = queryTarget.resolveTemplate("attr", "color")
+		        .resolveTemplate("value", Color.RED.name())
+		        .request(MediaType.APPLICATION_JSON).get();
+
+		final List<Car> redCarList = getResRed.readEntity(genCarList);
+		assertNotNull(redCarList);
+		assertEquals(1, redCarList.size());
+
+		// -- Retrieve blue cars - must be 2
+		final Response     getResBlue    = queryTarget.resolveTemplate("attr", "color")
+		        .resolveTemplate("value", Color.BLUE.name())
+		        .request(MediaType.APPLICATION_JSON).get();
+
+		// -- Using JsonArray to read value instead of generic type.
+		final List<String> blueCarIdList = getResBlue.readEntity(JsonArray.class).stream()
+		        .map(jco -> jco.asJsonObject().getString("id"))
+		        .collect(Collectors.toList());
+
+		assertNotNull(blueCarIdList);
+		assertEquals(2, blueCarIdList.size());
+
+		blueCarIdList.forEach(id -> System.out.println("id = " + id));
+
+	}
+}
